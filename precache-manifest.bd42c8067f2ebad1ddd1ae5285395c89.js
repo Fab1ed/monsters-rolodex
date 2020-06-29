caabf98985a6154dc19ac61d9567c0065ecd0c0e
Des <fab1ed@mail.ru>
2020-06-29 18:21:47 +0300

    Updates

---
+++ precache-manifest.bd42c8067f2ebad1ddd1ae5285395c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea0dab2066c3f12f81b221468106b51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eed46f17b6d41f8dfcb",
+    "url": "/static/css/main.952ca9f1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eed46f17b6d41f8dfcb",
+    "url": "/static/js/main.2deff2fb.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  }
+]);
